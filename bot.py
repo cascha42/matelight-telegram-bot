f210f677ac8e26b0c962019b2cd2587759b79863
cascha42 <c_ascha@c-base.org>
2019-01-14 18:37:43 +0100

    update bot.py

--- bot.py
+++ bot.py
@@ -3,54 +3,62 @@ import datetime
 
 import config
 
-class BotHandler:
+telegram_token = config.telegram_token 
+telegram_api_url = "https://api.telegram.org/bot{}/".format(telegram_token)
+#matelight_api_url = "http://matelight.cbrp3.c-base.org:80/api/"
+matelight_api_url = "http://localhost:1337/api/"
 
-    def __init__(self):
-        self.token = config.token 
-        self.api_url = "https://api.telegram.org/bot{}/".format(self.token)
+def telegram_send_message(chat_id, text):
+    params = {'chat_id': chat_id, 'text': text}
+    method = 'sendMessage'
+    resp = requests.post(telegram_api_url + method, params)
+    return resp
 
-    def get_updates(self, offset=None, timeout=30):
-        method = 'getUpdates'
-        params = {'timeout': timeout, 'offset': offset}
-        resp = requests.get(self.api_url + method, params)
-        result_json = resp.json()['result']
-        return result_json
+def get_updates(offset=None, timeout=30):
+    method = 'getUpdates'
+    params = {'timeout': timeout, 'offset': offset}
+    resp = requests.get(telegram_api_url + method, params)
+    result_json = resp.json()['result']
+    return result_json
 
-    def send_message(self, chat_id, text):
-        params = {'chat_id': chat_id, 'text': text}
-        method = 'sendMessage'
-        resp = requests.post(self.api_url + method, params)
-        return resp
+def get_last_update():
+    get_result = get_updates()
 
-    def get_last_update(self):
-        get_result = self.get_updates()
+    if len(get_result) > 1:
+        last_update = get_result[-1]
+    else:
+        last_update = get_result[len(get_result)]
 
-        if len(get_result) > 1:
-            last_update = get_result[-1]
-        else:
-            last_update = get_result[len(get_result)]
-
-        return last_update
-
-bot = BotHandler( )
+    return last_update
 
 def bot_help( chatid, args ):
     msg = """
 Commands:
+    list - list videos
     play [video_id] - play a video
     help - print this help screen
     """
     if args[0] != "help":
         msg = "Unknown command: '{}'\n".format( args[0] ) + msg
-    bot.send_message( chatid, msg )
+    telegram_send_message( chatid, msg )
 
 def bot_play( chatid, args ):
     msg = "playing {}".format( args[1] )
-    bot.send_message( chatid, msg )
+    telegram_send_message( chatid, msg )
+    resp = requests.get(matelight_api_url + "play/" + args[1])
+
+def bot_list( chatid, args ):
+    resp = requests.get(matelight_api_url + "getvideos").json()
+    videos=[]
+    for item in resp:
+        videos.append(item["title"])
+    msg = "available videos:\n  " + ",\n  ".join(videos)
+    telegram_send_message( chatid, msg )
 
 bot_toplevel = {
     "help": bot_help,
     "play": bot_play,
+    "list": bot_list,
     "default": bot_help
 }
 
@@ -62,8 +70,7 @@ def docmd( funcs, chatid, request ):
     return f( chatid, request )
 
 if __name__ == "__main__":
-    bot = BotHandler()
-    chatid = bot.get_last_update()["message"]["chat"]["id"]
-    msg = bot.get_last_update()["message"]["text"]
+    chatid = get_last_update()["message"]["chat"]["id"]
+    msg = get_last_update()["message"]["text"]
     print( "got {}".format( msg ) )
     docmd( bot_toplevel, chatid, msg.split() )
